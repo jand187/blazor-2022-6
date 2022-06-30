abd794126b3e5a83455c80e00f81751916aeb2c5
Jan Daniel Andersen <jan.andersen@visma.com>
2022-06-30 10:48:00 +0200

    Working signalr messageHub.

---
+++ client-console/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.AspNetCore.SignalR.Client;
+
+Console.WriteLine("Hello, World!");
+
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("https://localhost:7005/messagehub")
+    .Build();
+
+connection.Closed += async error =>
+{
+    await Task.Delay(new Random().Next(0, 5) * 1000);
+    await connection.StartAsync();
+};
+
+await connection.StartAsync();
+
+
+var message = "";
+
+while (message != "quit")
+{
+    message = Console.ReadLine();
+
+    await connection.InvokeAsync("SendMessage", "jand187", message);
+}
