ed345432a0e0588833220fcca597507d2331379a
Jan Daniel Andersen <jand187@gmail.com>
2022-07-01 10:55:56 +0200

    sdf

---
+++ web/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web.Model;
+
+public class Person
+{
+	public string Firstname { get; set; }
+	public string Lasstname { get; set; }
+	public Email Email{ get; set; }
+}
+
+public class Email
+{
+	[EmailAddress]
+	public string Address { get; set; }
+}
